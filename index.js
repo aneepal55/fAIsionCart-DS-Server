430ff89c552e4d125d44922a47b9f65458ac4372
Aneesh <yaagnik.p@gmail.com>
2024-10-26 20:37:40 -0400

    reduce data

--- index.js
+++ index.js
@@ -19,7 +19,19 @@ app.use(express.json());
 // Retrieve all products
 app.get('/api/products', async (req, res) => {
     try {
-        const products = await db.collection('products').find().toArray();
+        const products = await db.collection('products').find({}, {
+            projection: {
+                title: 1,
+                description: 1,
+                "images": { $slice: 1 }, // Get only the first image URL
+                category_name: 1,
+                color: 1,
+                "sizes.attr_value_name": 1, // Get only the 'attr_value_name' field for each size
+                "sale_price.amount_with_symbol": 1,// Get only the 'amount_with_symbol' field for the sale price
+                url: 1,
+            }
+        }).toArray();
+
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json({ message: 'Error retrieving products', error });
